0657e872d06916530c76690fc561937528fb8464
Benjamin Tatum <mail@benjtatum.com>
2022-10-16 07:23:08 -0400

    fix: remove unintended line

--- packages/engine/src/entities/__tests__/Collection.test.ts
+++ packages/engine/src/entities/__tests__/Collection.test.ts
@@ -12,7 +12,6 @@ const ElementFactory = () => container.get<Element>("Element");
 describe("Collection", () => {
   describe("options defaults", () => {
     let collection: Collection;
-
     beforeEach(() => {
       collection = CollectionFactory();
       collection.create([], {});
