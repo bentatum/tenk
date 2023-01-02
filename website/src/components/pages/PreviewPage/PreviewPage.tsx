b2de2ce37ab90158c739f6542dca37942c93f0cd
Benjamin Tatum <mail@benjtatum.com>
2023-01-02 13:53:55 -0500

    chore: remove boilerplate

--- website/src/components/pages/PreviewPage/PreviewPage.tsx
+++ website/src/components/pages/PreviewPage/PreviewPage.tsx
@@ -1,4 +1,3 @@
-// import metadata from "@/data/metadata.json";
 import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
 import Button from "@/components/Button";
@@ -7,16 +6,17 @@ import { useMemo, useState } from "react";
 import useSwr from "swr";
 import type { Metadata } from "@tenk/engine";
 
-// interface Attribute {
-//   trait_type: string;
-//   value: string;
-// }
-
 type Filters = Map<string, string[] | undefined>;
 
 const PreviewPage = () => {
-  const { data: metadata = [], error, isLoading } = useSwr<Metadata[]>('https://racers-dev-2.s3.us-east-1.amazonaws.com/2/metadata.json', (url) => fetch(url).then((res) => res.json()));
-  console.log(metadata, error, isLoading)
+  const {
+    data: metadata = [],
+    error,
+    isLoading,
+  } = useSwr<Metadata[]>(
+    "https://racers-dev-2.s3.us-east-1.amazonaws.com/2/metadata.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   const attributeRecord = metadata.reduce<Record<string, string[]>>(
     (acc, next) => {
@@ -30,7 +30,7 @@ const PreviewPage = () => {
     },
     {}
   );
-  
+
   const attributeNames = Object.keys(attributeRecord).sort();
   const [filters, setFilters] = useState<Filters>(new Map());
 
@@ -65,75 +65,75 @@ const PreviewPage = () => {
 
   return (
     <div>
-    <div className="grid grid-cols-12">
-      <div className="col-span-4">
-        <div className="sticky top-0 overflow-auto h-screen p-2">
-          {attributeNames.map((key) => (
-            <Disclosure key={key}>
-              {({ open }) => {
-                const Icon = open ? ChevronUpIcon : ChevronDownIcon;
-                const elements = attributeRecord[key];
-                return (
-                  <>
-                    <Disclosure.Button
-                      as={Button}
-                      className="w-full flex items-center justify-between normal-case"
-                    >
-                      {key}
-                      <div className="flex items-center space-x-2">
-                        <div className="font-light text-sm text-gray-500">
-                          {elements.length}
+      <div className="grid grid-cols-12">
+        <div className="col-span-4">
+          <div className="sticky top-0 overflow-auto h-screen p-2">
+            {attributeNames.map((key) => (
+              <Disclosure key={key}>
+                {({ open }) => {
+                  const Icon = open ? ChevronUpIcon : ChevronDownIcon;
+                  const elements = attributeRecord[key];
+                  return (
+                    <>
+                      <Disclosure.Button
+                        as={Button}
+                        className="w-full flex items-center justify-between normal-case"
+                      >
+                        {key}
+                        <div className="flex items-center space-x-2">
+                          <div className="font-light text-sm text-gray-500">
+                            {elements.length}
+                          </div>
+                          <Icon className="h-4 w-4" />
                         </div>
-                        <Icon className="h-4 w-4" />
-                      </div>
-                    </Disclosure.Button>
-                    <Disclosure.Panel className="px-3">
-                      {elements.map((value) => (
-                        <div key={value}>
-                          <input
-                            type="checkbox"
-                            id={value}
-                            name={value}
-                            value={value}
-                            onClick={() => onTraitValueClick(key, value)}
-                          />
-                          <label htmlFor={value}>{value}</label>
-                        </div>
-                      ))}
-                    </Disclosure.Panel>
-                  </>
-                );
-              }}
-            </Disclosure>
-          ))}
-        </div>
-      </div>
-      <div className="col-span-8">
-        <div className="grid grid-cols-6">
-          {metadata.filter((token) => {
-              return filterKeys.every((filterKey) => {
-                const attribute = token.attributes.find(
-                  ({ trait_type, value }) => {
-                    return (trait_type || value) === filterKey;
-                  }
-                );
-                return filters.get(filterKey)?.includes(attribute?.value!);
-              });
-            })
-            .map(({ name }) => (
-              <div key={name}>
-                <Image
-                  src={`https://racers-dev-2.s3.us-east-1.amazonaws.com/2/png/${name}.png`}
-                  alt=""
-                  width="480"
-                  height="480"
-                />
-              </div>
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="px-3">
+                        {elements.map((value) => (
+                          <div key={value}>
+                            <input
+                              type="checkbox"
+                              id={value}
+                              name={value}
+                              value={value}
+                              onClick={() => onTraitValueClick(key, value)}
+                            />
+                            <label htmlFor={value}>{value}</label>
+                          </div>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  );
+                }}
+              </Disclosure>
             ))}
+          </div>
+        </div>
+        <div className="col-span-8">
+          <div className="grid grid-cols-6">
+            {metadata
+              .filter((token) => {
+                return filterKeys.every((filterKey) => {
+                  const attribute = token.attributes.find(
+                    ({ trait_type, value }) => {
+                      return (trait_type || value) === filterKey;
+                    }
+                  );
+                  return filters.get(filterKey)?.includes(attribute?.value!);
+                });
+              })
+              .map(({ name }) => (
+                <div key={name}>
+                  <Image
+                    src={`https://racers-dev-2.s3.us-east-1.amazonaws.com/2/png/${name}.png`}
+                    alt=""
+                    width="480"
+                    height="480"
+                  />
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-    </div>
-
     </div>
   );
 };
