d6286eb7c77d3665ec31f2fc6114bfcf19c46878
Ben <mail@benjtatum.com>
2025-01-11 10:33:10 -0500

    fix(png): call setup canvas (#71)
    * ci: fix actions
    * chore: upgrade node version

--- packages/cli/src/entities/PngFile/__tests__/create.test.ts
+++ packages/cli/src/entities/PngFile/__tests__/create.test.ts
@@ -6,7 +6,12 @@ import { PngFile } from "../PngFile";
 import fs from "fs";
 
 jest.mock("canvas", () => ({
-  createCanvas: jest.fn(),
+  createCanvas: jest.fn().mockReturnValue({
+    getContext: jest.fn().mockReturnValue({
+      drawImage: jest.fn()
+    }),
+    toBuffer: jest.fn().mockReturnValue(Buffer.from([])),
+  }),
   loadImage: jest.fn(),
 }));
 const mockedLoadImage = createMock(loadImage);
@@ -54,10 +59,6 @@ describe("PngFile.create", () => {
   beforeEach(async () => {
     mockedLoadImage.mockResolvedValue(img as any);
     pngFile = PngFileFactory();
-    pngFile.canvas = { toBuffer: jest.fn() } as any;
-    pngFile.canvasContext = {
-      drawImage: jest.fn(),
-    } as any;
   });
 
   describe("load path", () => {
@@ -100,4 +101,6 @@ describe("PngFile.create", () => {
     await pngFile.create(attributes, renderPath);
     expect(mockedLoadImage).toHaveBeenCalledTimes(attributes.length);
   });
+
+  it.todo("calls setupCanvas with the correct arguments");
 });
